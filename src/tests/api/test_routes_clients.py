c19cc1cbe1eb5fdd0fa1a2575e7dcc45b6ce54db
JokyOFC <personal_joky@outlook.com>
2025-08-21 21:47:00 -0300

    fix: Fixing event loop windows and client tests

--- src/tests/api/test_routes_clients.py
+++ src/tests/api/test_routes_clients.py
@@ -1,61 +1,87 @@
-import pytest
-from httpx import ASGITransport, AsyncClient
+import asyncio
+import pytest_asyncio
+from src.db.database import get_session
+from httpx import AsyncClient, ASGITransport
 from src.main import app
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 from uuid import uuid4
 
-transport = ASGITransport(app=app)
-base_url = "http://localhost:8000"
-route = "/clients"
+from src.config import settings
 
-@pytest.mark.asyncio
-async def test_list_clients():
-    
-    async with AsyncClient(transport=transport, base_url=base_url) as ac:
-        response = await ac.get(route + "/")
-        
+if hasattr(asyncio, "WindowsSelectorEventLoopPolicy"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+DATABASE_URL = settings.DATABASE_URL
+
+base_url = "http://test"
+route = "/clientes"
+
+
+@pytest_asyncio.fixture
+async def async_session():
+    engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+    async_session_maker = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session_maker() as session:
+        yield session
+
+
+@pytest_asyncio.fixture
+async def async_client(async_session: AsyncSession):
+    app.dependency_overrides[get_session] = lambda: async_session
+
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        yield client
+
+
+# Tests
+
+
+async def test_list_clients(async_client):
+    response = await async_client.get(route + "/")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
     assert data[0]["name"] == "João Silva"
-    
-@pytest.mark.asyncio
-async def test_get_client_by_id():
-    
-    async with AsyncClient(transport=transport, base_url=base_url) as ac:
-        
-        list_response = await ac.get(route)
-        client_id = list_response.json()[0]["id"]
-        
-        response = await ac.get(route + "/" + client_id)
-        
+
+
+async def test_get_client_by_id(async_client):
+    list_response = await async_client.get(f"{route}/")
+    assert list_response.status_code == 200
+    client = list_response.json()
+    assert len(client) > 0, "Não há clientes cadastrados para testar"
+
+    client_id = client[0]["id"]
+
+    response = await async_client.get(f"{route}/{client_id}")
+
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == client_id
 
-@pytest.mark.asyncio
-async def test_get_client_not_found():
-    
+
+async def test_get_client_not_found(async_client):
     fake_id = str(uuid4())
-    
-    async with AsyncClient(transport=transport, base_url=base_url) as ac:
-    
-        response = await ac.get(route + fake_id)
-    
+
+    response = await async_client.get(route + fake_id)
+
     assert response.status_code == 404
-    assert response.json()["detail"] == "Client not found"
-    
-@pytest.mark.asyncio
-async def test_create_client():
-    
+    assert response.json()["detail"] == "Not Found"
+
+
+async def test_create_client(async_client):
     payload = {"name": "Carlos Pereira", "email": "carlos@example.com"}
-    
-    async with AsyncClient(transport=transport, base_url=base_url) as ac:
-        response = await ac.post(route + "/", json=payload)
-        
-        new_id = await ac.get(route)
-    
-    assert response.status_code == 200
+
+    response = await async_client.post(route + "/", json=payload)
+
+    new_id = await async_client.get(route + "/")
+    clients = new_id.json()
+
+    assert response.status_code == 201
     data = response.json()
-    new_id_len = len(new_id)
-    assert data["id"] == new_id[new_id_len - 1]["id"]
+    last_client = clients[-1]
+    assert data["id"] == last_client["id"]
     assert data["name"] == "Carlos Pereira"
