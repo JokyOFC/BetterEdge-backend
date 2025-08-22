7e95a667cbf6a407c32c5b4080d23ac708c59b3e
JokyOFC <personal_joky@outlook.com>
2025-08-21 21:52:01 -0300

    fix: Asyncronous Engine

--- src/db/database.py
+++ src/db/database.py
@@ -1,8 +1,20 @@
-from sqlalchemy import create_engine
+from typing import AsyncGenerator
 from sqlalchemy.orm import sessionmaker
 from src.config import settings
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+async_session = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
