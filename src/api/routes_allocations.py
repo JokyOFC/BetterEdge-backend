7b6fca4b651523d5919d67f306115e0de14292a0
JokyOFC <personal_joky@outlook.com>
2025-08-23 13:01:37 -0300

    feat: Routes uptodate

--- src/api/routes_allocations.py
+++ src/api/routes_allocations.py
@@ -2,17 +2,21 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/")
 def list_allocations():
-    return
+    return {}
+
 
 @router.post("/")
 def create_allocations():
     return
 
+
 @router.put("/")
 def update_allocations():
     return
+
 
 @router.delete("/")
 def delete_allocations():
--- src/api/routes_daily_returns.py
+++ src/api/routes_daily_returns.py
@@ -2,17 +2,21 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/")
 def list_daily_returns():
     return
+
 
 @router.post("/")
 def create_daily_returns():
     return
 
+
 @router.put("/")
 def update_daily_returns():
     return
+
 
 @router.delete("/")
 def delete_daily_returns():
