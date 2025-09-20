cfbdb2717960a7f5fe6187494a29db00fe373721
JokyOFC <personal_joky@outlook.com>
2025-09-20 19:12:33 -0300

    feat: creating crud routes for allocation and assets

--- src/api/routes_allocations.py
+++ src/api/routes_allocations.py
@@ -1,16 +1,99 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel, field_validator
+from typing import List
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.db.database import get_session
+from src.db.models.core_models import Allocation
 
 router = APIRouter()
 
+# Refactor schemes for DB
+class AllocationBase(BaseModel):
+    client_id: UUID
+    asset_id: UUID
+    quantity: float
+    avg_price: float
+    ticker: str | None
+    invested_amount: float
+    
+    @field_validator("ticker")
+    @classmethod
+    def normalize_ticker(cls, v: str) -> str:
+        return isinstance(v, str) and v.strip().upper() or ""
+    
 
-@router.get("/")
-def list_allocations():
-    return {}
+class AllocationOut(AllocationBase):
+    id: UUID
+    
+    class config:
+        orm_type = True
 
+@router.get("/", response_model=List[AllocationOut])
+async def list_allocations(session: AsyncSession = Depends(get_session)):
+    
+    stmt = select(Allocation)
+    result = await session.execute(stmt)
+    allocations = result.scalars().all()
+    
+    return allocations
 
-@router.post("/")
-def create_allocations():
-    return
+@router.get("/{allocation_id}", response_model=AllocationOut)
+async def list_allocation_by_id(allocation_id,  session: AsyncSession = Depends(get_session)):
+    stmt = select(Allocation).where(Allocation.id.in_([allocation_id]))
+    result = await session.execute(stmt)
+    allocation = result.scalars().first()
+    if not allocation:
+        raise HTTPException(status_code=404, detail="Not Found")
+    
+    return allocation
+
+@router.get("/{client_id}", response_model=List[AllocationOut])
+async def list_allocation_by_client_id(client_id,  session: AsyncSession = Depends(get_session)):
+    stmt = select(Allocation).where(Allocation.client_id.in_([client_id]))
+    result = await session.execute(stmt)
+    allocation = result.scalars().first()
+    if not allocation:
+        raise HTTPException(status_code=404, detail="Not Found")
+    
+    return allocation
+
+@router.post("/", response_model=AllocationOut, status_code=status.HTTP_201_CREATED)
+async def create_allocations(allocation:AllocationBase, session: AsyncSession = Depends(get_session)):
+
+    if not allocation.client_id:
+        raise HTTPException(status_code=status.HTTP_400, detail="Client is required.")
+    
+    if not allocation.asset_id:
+        raise HTTPException(status_code=status.HTTP_400, detail="Asset is required.")
+    
+    if not isinstance(allocation.client_id, UUID):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client must be a UUID.")
+    
+    if not isinstance(allocation.asset_id, UUID):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Asset must be a UUID.")
+    
+    if not allocation.quantity or not allocation.avg_price or not allocation.invested_amount:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Values must be filled.")
+
+    if allocation.quantity < 0 or allocation.avg_price < 0 or allocation.invested_amount < 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Values cannot be negative.")
+    
+    new_allocation = Allocation(
+        client_id=allocation.client_id,
+        asset_id=allocation.asset_id,
+        quantity=allocation.quantity,
+        avg_price=allocation.avg_price,
+        invested_amount=allocation.invested_amount,
+        ticker=allocation.ticker,
+    )
+    
+    session.add(new_allocation)
+    await session.commit()
+    await session.refresh(new_allocation)
+    
+    return new_allocation
 
 
 @router.put("/")
--- src/api/routes_assets.py
+++ src/api/routes_assets.py
@@ -1,16 +1,96 @@
-from fastapi import APIRouter
+import enum
+#import yfinance as yahoo
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+from uuid import UUID
+from src.db.database import get_session
+from src.db.models.core_models import Asset
+from sqlalchemy import select
+
 
 router = APIRouter()
 
+class AssetTypeEnum(str, enum.Enum):
+    ACAO = "acao"
+    FII = "fii"
+    FUNDO = "fundo"
+    RENDA_FIXA = "renda_fixa"
 
-@router.get("/")
-def list_assets():
-    return
+class AssetBase(BaseModel):
+    name: str
+    asset_type: AssetTypeEnum
+    currency: str
+    default_fee_rate: float
+    has_dividend: bool
+    ticker: str
+    
 
+class AssetOut(AssetBase):
+    id: UUID
+    
+    class config:
+        orm_mode = True
 
-@router.post("/")
-def create_assets():
-    return
+@router.get("/", response_model=List[AssetOut])
+async def list_assets(session: AsyncSession = Depends(get_session)):
+    stmt = select(Asset)
+    result = await session.execute(stmt)
+    assets = result.scalars().all()
+    return assets
+
+@router.get("/{asset_id}", response_model=AssetOut)
+async def get_asset_by_id(asset_id, session: AsyncSession = Depends(get_session)):
+    stmt = select(Asset).where(Asset.id.in_([asset_id]))
+    result = await session.execute(stmt)
+    asset = result.scalar().first()
+    
+    if not asset:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    
+    return asset
+
+@router.get("/ticker/{asset_ticker}", response_model=AssetOut)
+async def get_asset_by_ticker(asset_ticker, session: AsyncSession = Depends(get_session)):
+    stmt = select(Asset).where(Asset.ticker.in_([asset_ticker]))
+    result = await session.execute(stmt)
+    asset = result.scalars().first()
+    
+    if not asset:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    
+    return asset
+
+@router.post("/", response_model=AssetOut, status_code=status.HTTP_201_CREATED)
+async def create_assets(asset: AssetBase, session: AsyncSession = Depends(get_session)):
+    
+    """
+        TODO: Verify if ticker is already registred
+    """
+    if not asset.ticker:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Ticker is required")
+    
+    if not asset.name:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Name is required")
+    
+    if not asset.asset_type:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Asset type is required")
+    
+    new_asset = Asset(
+        name=asset.name,
+        ticker=asset.ticker,
+        asset_type=asset.asset_type,
+        currency=asset.currency,
+        default_fee_rate=asset.default_fee_rate,
+        has_dividend=asset.has_dividend
+    )
+    
+    session.add(new_asset)
+    await session.commit()
+    await session.refresh(new_asset)
+    
+    return new_asset
 
 
 @router.put("/")
